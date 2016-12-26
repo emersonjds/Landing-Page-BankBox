dae74cf91dd02cc5485f8d0288a61ae69df9e295
Emerson Jr <emerson_jds@live.com>
2016-12-26 12:02:40 -0200

    Alteração do texto da area de caracteristicas e implementação do ajax para requisições no servidor e armazenamento de dados

--- assets/js/newScript.js
+++ assets/js/newScript.js
@@ -1,7 +1,30 @@
 $(document).ready(function () {
+
+	var date = new Date ();
+	var contClickAndroid = 0;
+	var contClickIos = 0;
+
+
 	$('.btn-android').click(function(event) {
 		event.preventDefault();
 		$('#dados-de-usuario-android').modal();
+
+		var dia = date;
+		contClickAndroid++;
+
+		$.ajax({
+			url: '/path/to/file',
+			type: 'POST',
+			data: {
+				email: $('input[name=email]').val()	
+			}
+		}, 
+		success = function() {
+
+		}, 
+		error = function(e) {
+
+		})
 	});
 
 	$('.btn-ios').click(function(event) {
